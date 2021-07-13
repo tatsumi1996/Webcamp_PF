0860ab22758e77632ae3fa19245b18f783582fe3
tatsumi1996 <dannsamyu@gmail.com>
2021-07-13 14:55:49 +0000

    ビューページの修正

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
     before_action :authenticate_admin!
 
 	def index
-    @uses = User.all.page(params[:page]).per(10)
+    @users = User.all.page(params[:page]).per(10)
 	end
 
 	def show
