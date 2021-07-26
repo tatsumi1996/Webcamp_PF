5bf4c31ea2d7203e8524a4b89eb98154d9360f49
tatsumi1996 <dannsamyu@gmail.com>
2021-07-26 16:32:17 +0000

    eventsの登録を開始時刻>終了時刻で登録不可

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validate :start_end_check
+
+  def start_end_check
+    errors.add(:end_date, "は開始時刻より遅い時間を選択してください") if self.start_date > self.end_date
+  end
+
 end
