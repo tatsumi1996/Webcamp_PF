56f5d8ac2822109ce2b9bfc43180655a3161bb85
tatsumi1996 <dannsamyu@gmail.com>
2021-08-07 17:06:21 +0000

    no methodエラー解消

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -8,7 +8,7 @@ class HomesController < ApplicationController
     def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
-      user.name = "ゲスト"
+      user.nickname = "ゲスト"
       end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
